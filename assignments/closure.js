ce794eed0a8287342265c9b52efe22d558ded9cf
Rasha Moumneh <r.moumneh@gmail.com>
2019-04-19 22:23:11 -0400

    JS II assignment

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,21 +1,31 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
+let num = 1;
+function numberGenerator() {
+  function logNumber() { 
+    console.log(num);
+  }
+  num++;
+  return logNumber;
+}
 
+let number = numberGenerator();
+number(); // 2
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
 
 // ==== Challenge 2: Create a counter function ====
-const counter = () => {
+// const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
-};
+// };
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
-const counterFactory = () => {
+// const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
-};
+// };
